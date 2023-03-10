b949a809c338253c9b76c6052e67c06191fd6cce
s2435255 <s2435255@ed.ac.uk>
2023-03-10 19:25:34 +0000

    Changing to retrieve from mongo db

--- project/todo/views.py
+++ project/todo/views.py
@@ -75,18 +75,12 @@ class RecipeSearch:
       rsl = RankSearch.rankByBM25(query, index_obj)
     elif (search_type == "other"):
       rsl = BooleanSearch.boolean_search(query, index_obj)
-    output_str = "::::: Retreived Document IDs :::::: \n"
-    """""
-    recipes = {"results" : []}
-    recipe_data = RecipeData.getInstance()
-    recipes = recipe_data.get_multiple_recipes(list(rsl.keys()))
+
 
     if rsl != None:
+      recipes = {"results" : []}
+      recipe_data = RecipeData.getInstance()
       for did in rsl:
-      # for did in rsl.keys():
-        did = str(did-1)
-        print("did: ",did)
-        recipe_data = RecipeData.getInstance()
         title = recipe_data.get_recipe_fields(did)[0]
         ingredients = recipe_data.get_recipe_fields(did)[1]
         directions = recipe_data.get_recipe_fields(did)[2]     
@@ -95,60 +89,42 @@ class RecipeSearch:
       recipes_object = json.dumps(recipes) 
       print(recipes_object)
     else:
-      print ("Sorry! No search results found")
-    return HttpResponse(recipes_object) 
-    
-      recipe_data = RecipeData.getInstance()
-      #If using Mongo DB fetch data
-      # recipes = recipe_data.get_multiple_recipes(list(rsl.keys()))
-      # output_str = ""
-      # for data in recipes:
-      #   output_str += str(data['id']) + data['title'] + data['ingredients'] + data['directions']
-      ## Shivaz: Need for testing
-      for did in rsl.keys():
-        #Single MongoDB calls not efficient
-        # title = recipe_data.get_recipe_fields(did)[0]
-        # output_str += str(did) + title + "\n"
-        output_str += str(did) + "\n"
-
-    else:
-      print ("Sorry! No search results found")
-    print("Time taken for tfidf search: ", time.time() - start)
-    return HttpResponse(output_str) 
-   """
-
-  def searchQueryResult(request):
-    print ("Request method :", request.method)
-    query = request.GET.get("query")
-    print(query)
-    index_obj = RecipeSearch.getInstance().getRecipeIndexObj()
-    start = time.time()
-    if (search_type == "tfidf"):
-      rsl = RankSearch.rankByTFIDF(query, index_obj)
-    elif (search_type == "bm25"):
-      rsl = RankSearch.rankByBM25(query, index_obj)
-    elif (search_type == "other"):
-      rsl = BooleanSearch.boolean_search(query, index_obj)
-    if rsl!=None:
-      recipes = {"results" : []}
-      recipe_data = RecipeData.getInstance()
-      rsl_list = [str(i-1) for i in list(rsl.keys())]
-      print("rsl_list: ",rsl_list)
-      recipes_cursor = recipe_data.get_multiple_recipes(rsl_list)
-      #print("recipe_cursor: ",recipes_cursor)
-      #for data in recipes_cursor:
-        #print("data: ",data)
-           
-      list_cursor = list(recipes_cursor)
-      print("List cursor: ",list_cursor)
-      recipes["results"] = dumps(list_cursor, indent = 2)
-      recipes_object = json.dumps(recipes)
-      #print("List cursor: ",list_cursor)
-      print("Recipes_object: ",recipes_object)
-    else:
       recipes_object = json.dumps({})
       print ("Sorry! No search results found")
     return HttpResponse(recipes_object) 
+
+  # def searchQueryResult(request):
+  #   print ("Request method :", request.method)
+  #   query = request.GET.get("query")
+  #   print(query)
+  #   index_obj = RecipeSearch.getInstance().getRecipeIndexObj()
+  #   start = time.time()
+  #   if (search_type == "tfidf"):
+  #     rsl = RankSearch.rankByTFIDF(query, index_obj)
+  #   elif (search_type == "bm25"):
+  #     rsl = RankSearch.rankByBM25(query, index_obj)
+  #   elif (search_type == "other"):
+  #     rsl = BooleanSearch.boolean_search(query, index_obj)
+  #   if rsl!=None:
+  #     recipes = {"results" : []}
+  #     recipe_data = RecipeData.getInstance()
+  #     # rsl_list = [str(i-1) for i in list(rsl.keys())]
+  #     # print("rsl_list: ",rsl_list)
+  #     recipes_cursor = recipe_data.get_multiple_recipes(list(rsl.keys()))
+  #     # print("recipe_cursor: ",recipes_cursor)
+  #     # for data in recipes_cursor:
+  #     #   print("data: ",data)
+           
+  #     list_cursor = list(recipes_cursor)
+  #     # print("List cursor: ",list_cursor)
+  #     recipes["results"] = dumps(list_cursor, indent = 2)
+  #     recipes_object = json.dumps(recipes)
+  #     #print("List cursor: ",list_cursor)
+  #     print("Recipes_object: ",recipes_object)
+  #   else:
+  #     recipes_object = json.dumps({})
+  #     print ("Sorry! No search results found")
+  #   return HttpResponse(recipes_object) 
 
 
       
